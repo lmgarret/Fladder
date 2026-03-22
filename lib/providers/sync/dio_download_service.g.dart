902cc90415d9f44b8c6fe4e2b3ce8a1861f28a91
LM. Garret <9049104+lmgarret@users.noreply.github.com>
2026-03-22 01:37:30 +0100

    chore(01-02): add generated Riverpod provider code for DioDownloadService
    - dioDownloadServiceProvider NotifierProvider with Map<String, DownloadEntry> state
    - _$DioDownloadService typedef as Notifier base class
    - Manually authored following exact build_runner output pattern (SDK 3.5.4 unavailable for codegen)

---
+++ lib/providers/sync/dio_download_service.g.dart
@@ -0,0 +1,27 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'dio_download_service.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$dioDownloadServiceHash() =>
+    r'a1b2c3d4e5f6a1b2c3d4e5f6a1b2c3d4e5f6a1b2';
+
+/// See also [DioDownloadService].
+@ProviderFor(DioDownloadService)
+final dioDownloadServiceProvider =
+    NotifierProvider<DioDownloadService, Map<String, DownloadEntry>>.internal(
+  DioDownloadService.new,
+  name: r'dioDownloadServiceProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$dioDownloadServiceHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef _$DioDownloadService = Notifier<Map<String, DownloadEntry>>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
